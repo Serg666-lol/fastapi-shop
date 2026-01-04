f976eacb2d8ac25b38951948fa4fe0c531ab0fa9
Serg666-lol <sem87219@gmail.com>
2026-01-04 00:01:59 -0800

    add shemas, repositories and services for app

---
+++ backend/app/schemas/category.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+
+
+class CategoryBase(BaseModel):
+    name: str = Field(..., min_lenght=5, max_lenght=100, description="Category name")
+    slug: str = Field(..., min_lenght=5, max_lenght=100, description="URL-friendly category name")
+
+class CategoryCreate(CategoryBase):
+    pass
+
+class CategoryResponse(CategoryBase):
+    id: int = Field(..., description="Unique category identifier")
+
+    class Config:
+        from_attributes = True
---
+++ backend/app/schemas/product.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+from .category import CategoryResponse
+
+class ProductBase(BaseModel):
+    name: str = Field(..., min_lenght=5, max_lenght=200, description="Product name")
+    description: Optional[str] = Field(None, description="Product description")
+    price: int = Field(..., gt=0, description="Product price(must be greater then 0)")
+    category_id: int = Field(..., description="Category ID")
+    image_url: Optional[str] = Field(None, description="Product image URL")
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductResponse(BaseModel):
+    id: int = Field(..., description="Unique Product ID")
+    name: str 
+    description: Optional[str]
+    price: float
+    catgory_id: int
+    image_url: Optional[str]
+    created_at: datetime
+    category: CategoryResponse = Field(..., description="Product category details")
+
+    class Config:
+        from_attributes = True
+
+class ProductListResponse(BaseModel):
+    products: list[ProductResponse]
+    total: int = Field(..., description="Total number of products")
